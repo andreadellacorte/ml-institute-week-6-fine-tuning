fe1172bee7fc3ccfc3d687202b9d113692fc1720
andreadellacorte <andrea@dellacorte.me>
2025-05-20 10:36:18 +0000

    feat: stab 2 - lora

--- setup.py
+++ setup.py
@@ -5,11 +5,4 @@ setup(
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[
-        "peft>=0.15.2",
-        "trl>=0.17.0",
-        "torch>=2.0.0",
-        "transformers>=4.51.3",
-        "datasets>=3.6.0",
-    ],
 )
